3e6c72acd003178a17c1ed3da1989f8f5df3fdc4
alextts627 <alextts627@gmail.com>
2018-04-11 14:48:17 +0800

    add function

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
- 
+import TodoItems from "./TodoItems";
+
 class TodoList extends Component {
     constructor(props) {
         super(props);
@@ -42,6 +43,7 @@ class TodoList extends Component {
             <button type="submit">add</button>
           </form>
         </div>
+        <TodoItems entries={this.state.items}/>
       </div>
     );
   }

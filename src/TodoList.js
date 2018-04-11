002e29ac1f0530a0316560f9a61ee502f8038a45
alextts627 <alextts627@gmail.com>
2018-04-11 15:46:33 +0800

    adding 'deleteItem' function, FlipMove libarary to have annimation

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import TodoItems from "./TodoItems";
+import "./TodoList.css";
 
 class TodoList extends Component {
     constructor(props) {
@@ -10,6 +11,8 @@ class TodoList extends Component {
           };
         
         this.addItem = this.addItem.bind(this);
+        this.deleteItem = this.deleteItem.bind(this);
+
       }
       addItem(e) {
           
@@ -32,18 +35,30 @@ class TodoList extends Component {
            
         e.preventDefault();
       }
+
+      deleteItem(key) {
+        var filteredItems = this.state.items.filter(function (item) {
+          return (item.key !== key);
+        });
+       
+        this.setState({
+          items: filteredItems
+        });
+      }
+
   render() {
     return (
       <div className="todoListMain">
         <div className="header">
           <form onSubmit = {this.addItem}>
-          <input ref={(a) => this._inputElement = a} placeholder="enter task">
-
+            <input ref={(a) => this._inputElement = a} 
+              placeholder="enter task">
             </input>
             <button type="submit">add</button>
           </form>
         </div>
-        <TodoItems entries={this.state.items}/>
+        <TodoItems entries={this.state.items}
+          delete={this.deleteItem}/>
       </div>
     );
   }
